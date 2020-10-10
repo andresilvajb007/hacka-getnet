f3be3f3257f16e0e848296fbddef28eb683c848f
andre silva <andre.silvajb@gmailcom>
2020-10-10 19:36:06 -0300

    adicionando url da imagem da solicitacao de credito e quantidade de parcelas de reembolso

---
+++ hacka-getnet/Migrations/20201010222820_Adiciona_Parcelas_UrlImagem_SolicitacaoCredito.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace hacka_getnet.Migrations
+{
+    public partial class Adiciona_Parcelas_UrlImagem_SolicitacaoCredito : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "QuantidadeParcelasReembolso",
+                table: "SolicitacaoCredito",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UrlImagem",
+                table: "SolicitacaoCredito",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "QuantidadeParcelasReembolso",
+                table: "SolicitacaoCredito");
+
+            migrationBuilder.DropColumn(
+                name: "UrlImagem",
+                table: "SolicitacaoCredito");
+        }
+    }
+}
